f43ddb255c8223edf46ba38f19b91363f2c03f20
Niranjan <niranjan.thanikachalam@outlook.com>
2017-02-22 22:21:35 +0000

    Oops

---
+++ config.py
@@ -0,0 +1,7 @@
+# configuration
+DATABASE = 'static/exposr.db'
+DEBUG = False
+SECRET_KEY = 'sleeponthefloor'
+USERNAME = 'oculonterrin'
+PASSWORD = 'sleeponthefloor'
+UPLOAD_FOLDER = 'static/uploads/'
